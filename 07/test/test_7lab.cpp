530158ea1e133edef8774175ae3c28ebab0c75c6
Timofey <timofeez@mail.ru>
2024-12-08 19:18:28 +0000

    lab7: finished

--- 07/test/test_7lab.cpp
+++ 07/test/test_7lab.cpp
@@ -1,68 +1,116 @@
 #include <gtest/gtest.h>
-#include "dungeon.h"
 #include "npc.h"
+#include "dungeon.h"
+#include "factory.h"
 #include "observer.h"
-#include "visitor.h"
+#include "movement_manager.h"
+#include "battle_manager.h"
+#include "utils.h"
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <atomic>
 #include <sstream>
 
 
-TEST(NPCTest, CreateNPC) {
-    Squirrel squirrel("Squeaky", 10, 15);
-    Werewolf werewolf("Howler", 20, 25);
-    Druid druid("Gandalf", 30, 35);
-
-    EXPECT_EQ(squirrel.getName(), "Squeaky");
-    EXPECT_EQ(werewolf.getX(), 20);
-    EXPECT_EQ(druid.getY(), 35);
-}
-
-
-
-
-TEST(NPCTest, GetType) {
-    Squirrel squirrel("Squeaky", 10, 15);
-    Werewolf werewolf("Howler", 20, 25);
-    Druid druid("Gandalf", 30, 35);
-    EXPECT_EQ(squirrel.getType(), "Squirrel");
-    EXPECT_EQ(werewolf.getType(), "Werewolf");
-    EXPECT_EQ(druid.getType(), "Druid");
-}
-
-
-TEST(DungeonTest, AddNPC) {
+TEST(MovementManagerTest, MoveNPCs) {
     Dungeon dungeon;
-    auto squirrel = std::make_shared<Squirrel>("Squeaky", 10, 15);
-    dungeon.addNPC(squirrel);
-    EXPECT_EQ(dungeon.popNPC(0)->getName(), "Squeaky");
+    auto npc = std::make_shared<Squirrel>("Squirrel", 10, 10);
+    dungeon.addNPC(npc);
+
+    std::mutex battleQueueMutex;
+    std::queue<BattleTask> battleQueue;
+    std::atomic<bool> running(true);
+
+    MovementManager movementManager(dungeon, battleQueueMutex, battleQueue, running);
+
+    
+    EXPECT_EQ(npc->getX(), 10);
+    EXPECT_EQ(npc->getY(), 10);
+
+    
+    movementManager.start();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    running = false;
+    movementManager.join();
+
+    
+    EXPECT_NE(npc->getX(), 10);
+    EXPECT_NE(npc->getY(), 10);
 }
 
 
-TEST(DungeonTest, PrintNPCs) {
+
+TEST(FullScenarioTest, Integration) {
     Dungeon dungeon;
-    auto squirrel = std::make_shared<Squirrel>("Squeaky", 10, 15);
-    auto werewolf = std::make_shared<Werewolf>("Howler", 20, 25);
-    dungeon.addNPC(squirrel);
-    dungeon.addNPC(werewolf);
-    std::ostringstream output;
-    std::streambuf* old_buf = std::cout.rdbuf(output.rdbuf());
-    dungeon.printNPCs();
-    std::cout.rdbuf(old_buf);
-    EXPECT_NE(output.str().find("Squirrel"), std::string::npos);
-    EXPECT_NE(output.str().find("Howler"), std::string::npos);
+    auto consoleObserver = std::make_shared<ConsoleObserver>();
+    auto fileObserver = std::make_shared<FileObserver>("test_log.txt");
+    std::vector<std::shared_ptr<Observer>> observers = {consoleObserver, fileObserver};
+
+    std::mutex battleQueueMutex;
+    std::queue<BattleTask> battleQueue;
+    std::atomic<bool> running(true);
+
+    SquirrelFactory squirrelFactory;
+    DruidFactory druidFactory;
+    WerewolfFactory werewolfFactory;
+
+    
+    for (int i = 0; i < 10; ++i) {
+        int x = getRandomNumber(0, 100);
+        int y = getRandomNumber(0, 100);
+        int type = getRandomNumber(0, 2);
+        std::shared_ptr<NPC> npc;
+        if (type == 0) {
+            npc = squirrelFactory.createNPC("Squirrel_" + std::to_string(i), x, y);
+        } else if (type == 1) {
+            npc = druidFactory.createNPC("Druid_" + std::to_string(i), x, y);
+        } else {
+            npc = werewolfFactory.createNPC("Werewolf_" + std::to_string(i), x, y);
+        }
+        dungeon.addNPC(npc);
+    }
+
+    
+    MovementManager movementManager(dungeon, battleQueueMutex, battleQueue, running);
+    BattleManager battleManager(dungeon, battleQueueMutex, battleQueue, *consoleObserver, running);
+
+    movementManager.start();
+    battleManager.start();
+
+    
+    std::thread printThread([&]() {
+        for (int i = 0; i < 5; ++i) {
+            std::stringstream ss;
+            ss << "Текущее состояние карты:\n";
+            dungeon.printNPCs(ss);
+            ss << "----------------------------------";
+            for (auto &observer : observers) {
+                observer->update(ss.str());
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        running = false;
+    });
+
+    printThread.join();
+    movementManager.join();
+    battleManager.join();
+
+    
+    std::stringstream ss;
+    dungeon.printNPCs(ss);
+    EXPECT_GT(ss.str().size(), 0);
 }
 
 
-TEST(DungeonTest, SaveAndLoadNPCs) {
-    Dungeon dungeon;
-    auto squirrel = std::make_shared<Squirrel>("Squeaky", 10, 15);
-    auto werewolf = std::make_shared<Werewolf>("Howler", 20, 25);
-    dungeon.addNPC(squirrel);
-    dungeon.addNPC(werewolf);
-    dungeon.saveToFile("test_npcs.txt");
-    Dungeon newDungeon;
-    newDungeon.loadFromFile("test_npcs.txt");
-    EXPECT_EQ(newDungeon.popNPC(0)->getName(), "Squeaky");
-    EXPECT_EQ(newDungeon.popNPC(0)->getName(), "Howler");
+TEST(UtilsTest, RandomNumber) {
+    int min = 10, max = 20;
+    for (int i = 0; i < 100; ++i) {
+        int random = getRandomNumber(min, max);
+        EXPECT_GE(random, min);
+        EXPECT_LE(random, max);
+    }
 }
 
 int main(int argc, char **argv) {
